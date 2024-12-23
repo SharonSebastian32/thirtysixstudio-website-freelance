bc313af5004243b65c9e057d09783a74a63be31c
Sharon Sebastian <125905370+SharonSebastian32@users.noreply.github.com>
2024-12-23 13:29:02 +0530

    one image animation added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Canvas from "./Canvas.jsx";
 function App() {
   return (
     <>
-      <div className="w-full min-h-screen bg-black text-white">
+      <div className="w-full min-h-screen bg-red-600 text-white">
         <Canvas />
       </div>
     </>
--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,21 +1,35 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import canvasImages from "./CanvasImages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 function Canvas() {
+  const [index, setIndex] = useState({ value: 0 });
   const canvasRef = useRef(null);
 
+  useGSAP(() => {
+    gsap.to(index, {
+      value: 149,
+      duration: 5,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+  });
+
   useEffect(() => {
-    const canvas = document.getElementById("canvas");
+    const canvas =  canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
-    img.src = canvasImages[80];
+    img.src = canvasImages[index.value];
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       ctx.drawImage(img, 0, 0);
     };
-  });
+  }, [index]);
   return (
-    <canvas id="canvas" ref={canvasRef} className="w-[18rem] has-[18rem] ">
+    <canvas id="canvas" ref={canvasRef} className="w-[18rem] h-[18rem]">
       {" "}
     </canvas>
   );
